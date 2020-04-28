e579ea553eae21a4aaa3f194f61ea340d7799499
filypsdias <lipao.dias@hotmail.com>
2020-04-28 19:14:24 -0300

    Adding Swagger Configuration

--- src/main/java/com/ais/challenge/moviemicroservice/MovieMicroserviceApplication.java
+++ src/main/java/com/ais/challenge/moviemicroservice/MovieMicroserviceApplication.java
@@ -2,7 +2,12 @@ package com.ais.challenge.moviemicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
+@EnableSwagger2WebMvc
+@Import(SpringDataRestConfiguration.class)
 @SpringBootApplication
 public class MovieMicroserviceApplication {
 
---
+++ src/main/java/com/ais/challenge/moviemicroservice/configuration/SwaggerConfiguration.java
@@ -0,0 +1,33 @@
+package com.ais.challenge.moviemicroservice.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Value("${project.version}")
+    private String version;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .enable(true)
+                .select()
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Movie Service API Documentation")
+                .version(version)
+                .build();
+    }
+
+}
