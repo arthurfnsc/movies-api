d322775971fa618c11083f8934efb9b19a00c4a6
filypsdias <lipao.dias@hotmail.com>
2020-04-29 12:39:49 -0300

    Adding GetLatest method on MovieRepository

--- src/main/java/com/ais/challenge/moviemicroservice/repository/MovieRepository.java
+++ src/main/java/com/ais/challenge/moviemicroservice/repository/MovieRepository.java
@@ -3,7 +3,12 @@ package com.ais.challenge.moviemicroservice.repository;
 import com.ais.challenge.moviemicroservice.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    @RestResource
+    Movie findTopByOrderByIdDesc();
+
 }
